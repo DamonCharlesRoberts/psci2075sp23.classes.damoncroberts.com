c8fe4c673dc4d72041078b64d3f1406b09a1691e
Damon C. Roberts <damon.roberts-1@colorado.edu>
2023-03-01 15:34:35 -0700

    final draft of bivariate regression stuff and initial draft of interactive inference stuff

---
+++ examples/bivariate_regression.R
@@ -0,0 +1,137 @@
+# Title: bivariate regression example
+
+# Notes:
+    #* Description:
+        #** Complete working example of doing a bivariate regression
+    #* Updated: 2023-03-01
+        #** by: dcr
+
+# Ensure working directory is set to psci_2075 folder on desktop
+    #* Check my working directory
+getwd() 
+    #* if my working directory is not set to psci_2075 folder, then change it
+setwd("~/Desktop/psci_2075")
+
+# Load data
+waffles <- read.csv(file = "waffles.csv")
+
+# Load packages
+library(ggplot2) # for making handy graphics
+library(modelsummary) # for making handy tables 
+library(magrittr) # for the nifty %>% operator
+library(stargazer) # for making an alternative table of regression output
+# Descriptive statistics (Exploratory data analysis)
+    #* Univariate descriptive statistics
+        #** Tabular
+waffles %>% # grab the waffles dataset
+    select( # select the columns I want to analyze
+        WaffleHouses,
+        Divorce
+    ) %>%
+    datasummary_skim( # make a table of univariate statistics
+        data = ., # use the dataset linked with the pipe operators
+        notes = "Data source: Waffles dataset (McElreath 2020).", # provide a caption for the table
+        output = "univariate_descriptives.docx" # save the output to this file
+    )
+        #** Graphical
+            #*** WaffleHouses column
+waffles %>% # grab the waffles dataset
+    ggplot() + # make a graph using the data
+    geom_histogram( # specifically a histogram
+        aes(x = WaffleHouses), # put the WaffleHouses variable on the x-axis
+        color = "#000000", # add black lines around the borders of the bars
+        fill = "#D3D3D3" # fill the bars with a light grey color
+    ) +
+    theme_minimal() +# clean up the theme to be prettier
+    labs( # change the labels
+        x = "Waffle Houses", # change x-axis label to this
+        y = "Count", # change y-axis label to this
+        caption = "Data source: Waffles dataset (McElreath 2020)."
+    )
+            #*** Divorce column
+waffles %>% # grab the waffles dataset
+    ggplot() + # make a graph using the data
+    geom_histogram( # specifically a histogram
+        aes(x = Divorce), # put the Divorce variable on the x-axis
+        color = "#000000", # add black lines around the borders of the bars
+        fill = "#D3D3D3" # fill the bars with a light grey color
+    ) +
+    theme_minimal() +# clean up the theme to be prettier
+    labs( # change the labels
+        x = "Divorce", # change x-axis label to this
+        y = "Count", # change y-axis label to this
+        caption = "Data source: Waffles dataset (McElreath 2020)."
+    )
+
+    #* Bivariate descriptive analyses
+waffles %>% # grab the waffles dataset
+    ggplot() + # make a graph using the data
+    geom_point( # specifically a scatterplot
+        aes(x = WaffleHouses, y = Divorce) # put WaffleHouses column on x-axis and Divorce column on y-axis
+        color = "#000000", # make color of dots black
+        fill = "#000000" # make fill color of dots black
+    ) +
+    theme_minimal() + # clean up the theme to be prettier
+    labs( # change the labels
+        x = "Waffle Houses?", # change x-axis label to this
+        y = "Divorce rate", # change y-axis label to this
+        caption = "Data source: Waffles dataset (McElreath 2020)." # add caption to plot
+    )
+# Regression
+    #* Visual (useful for first go at things, but not the final solution)
+waffles %>% # grab the waffles dataset
+    ggplot() + # make a graph using the data
+    geom_point( # specifically a scatterplot
+        aes(x = WaffleHouses, y = Divorce) # put WaffleHouses column on x-axis and Divorce column on y-axis
+        color = "#000000", # make color of dots black
+        fill = "#000000" # make fill color of dots black
+    ) +
+    geom_smooth(# add a line of best fit
+        method = lm, # line should be linear
+        color = "#000000", # make this line black
+        se = FALSE # do not include visual representation of uncertainty
+    ) +
+    theme_minimal() + # clean up the theme to be prettier
+    labs( # change the labels
+        x = "Waffle Houses?", # change x-axis label to this
+        y = "Divorce rate", # change y-axis label to this
+        caption = "Data source: Waffles dataset (McElreath 2020)." # add caption to plot
+    )
+    #* Tabular (official way to present regression results)
+        #** First calculate the model and store the result
+model <- lm( # calculate a linear model
+    data = Waffles, # use the waffles dataset
+    formula = Divorce ~ WaffleHouses # Divorce is the dependent variable and number of waffle houses is the independent variable
+)
+            #*** Can do a quick check of the model to see if it worked
+summary(model)
+            #*** can also look at our residuals if we want to understand how uncertain we are with the model
+waffles %>% # take the waffles data
+    ggplot() + # make a plot with it
+    geom_histogram( # specifically a histogram
+        aes(x = model$residuals), # and put the residuals from the model on the x-axis
+        color = "#000000", # make the lines between the bars black
+        fill = "#D3D3D3" # fill the bars with a light grey color
+    ) +
+    geom_vline( # add a vertical line
+        xintercept = mean(model$residuals), # the vertical line should be the mean value of my residuals
+        color = "#000000", # the color of the line should be black
+        linetype = 2 # and I want the line to be dotted
+    ) +
+    theme_minimal() + # apply the minimal theme to it
+    labs( # adjust some of the labels
+        x = "Residuals", # make x-axis label this
+        y = "Count", # make y-axis label this
+        caption = "Plot of residuals from model." # add this caption to the plot
+    )
+        #** Let's make a nice and easy to read output of the model's results
+            #*** Using the modelsummary function
+modelsummary(
+    model, # take the model and use it for the output 
+    output = "regression.docx" # store the result of the table into this file
+     )
+            #*** Using the stargazer function
+stargazer(
+    model, # take the model and use it for the output
+    type = "text" # make the type of output something that works nicely with a text file
+)
